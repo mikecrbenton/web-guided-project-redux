4d417623d80851b2a16b3b9027033a8598071421
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-08 13:33:32 -0500

    lecture work-in-progress

--- src/components/DragonList.js
+++ src/components/DragonList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { addMember } from './../actions/dragonActions';
+
 class DragonList extends React.Component {
   state = {
     newMember: ''
