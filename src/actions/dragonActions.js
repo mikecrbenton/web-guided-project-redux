8d25e4cb04bf89550cbba68a233bef536b134d28
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-08 14:01:45 -0500

    lecture work-in-progress

--- src/actions/dragonActions.js
+++ src/actions/dragonActions.js
@@ -4,5 +4,5 @@ export const ADD_MEMBER = "ADD_MEMBER";
 //action creator functions
 //  return action objects
 export const addMember = name => {
-    return({type: "ADD_A_MEMBER", payload:name});
+    return({type: ADD_MEMBER, payload:name});
 }
