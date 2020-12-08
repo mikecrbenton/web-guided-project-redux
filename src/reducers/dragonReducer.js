276fb271c74d355c59628daa7125bbe8a2e9725a
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-08 14:00:57 -0500

    lecture work-in-progress

--- src/reducers/dragonReducer.js
+++ src/reducers/dragonReducer.js
@@ -11,7 +11,7 @@ export const initialState = {
 
 export const dragonReducer = (state = initialState, action) => {
     switch(action.type) {
-        case(ADD_MEMBER):
+        case("ADD_MEMBER"):
             return({
                 ...state,
                 members: [...state.members, {name: action.payload, dragonStatus:false }]
